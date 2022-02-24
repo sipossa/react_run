8578214844b49a5b4844d809cbde29ac117cf418
leechanghu <sipossa09@gmail.com>
2022-02-24 21:09:37 +0900

    20220224

--- src/Section01.js
+++ src/Section01.js
@@ -1,4 +1,4 @@
-
+import React, { useRef, useEffect, useState } from "react";
 
 function Section01() {
     const detail = [
@@ -16,12 +16,31 @@ function Section01() {
         }
     ];
 
+
+    let containerRef = useRef(null);
+    const [title, setTitle] = useState("");
   
+    const myEvent = (idx) => {
+      console.log([...title],idx);
+      let cur = [...title];
+      cur.map(el => el.classList.remove('on'));
+      cur[idx].classList.add('on')
+    };
+  
+    useEffect(() => {
+      setTitle(containerRef.current.children);
+    }, []);
+
+
     const numIdx = (i) => {
-        console.log(i);
+        //const [navOpened, setNavOpened] = useState(false);
+        //const navClassNames = navOpened ? "nav-links nav-active" : "nav-links";
+        //console.log(i,nameInput.current,detailBox,containerRef.current.children[0].type);
         const ALLFIGURE = document.querySelectorAll('#content01 figure');
-        ALLFIGURE.forEach(el=>el.classList.remove('on'));
-        ALLFIGURE[i].classList.add('on')
+        [...ALLFIGURE].map(el=>el.classList.remove('on'));
+        [...ALLFIGURE][i].classList.add('on');
+
+       console.log([...ALLFIGURE],[...detail],[...detailBox][0])
 
     }
 
@@ -31,7 +50,7 @@ function Section01() {
                 <img src={el.imageUrl} alt="" />
             </div>
             <strong>{el.text}</strong>
-            <a href="#!" onClick={()=>numIdx(idx)}>구매하기</a>
+            <a href="#!" onClick={()=>myEvent(idx)}>구매하기</a>
         </figure>
     )
     );
@@ -39,7 +58,7 @@ function Section01() {
         <section id="content01">
             <h2>JEWELLERY COLLECTION</h2>
             <p>모던하면서도 클래식한 디자인들을 완벽하게 표현한 그라프 하우스의 시그너쳐 컬렉션은 다양한 컷</p>
-            <div className="container">
+            <div className="container" ref={containerRef}>
                 {detailBox}
             </div>
         </section>
